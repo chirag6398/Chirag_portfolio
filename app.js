0dbb0627749b033b3c31c4c9fd72ee21f10b4492
Chirag <agarwalchirag112@gmail.com>
2021-05-16 20:49:14 +0530

    combine frontend with backened

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
-
+const path = require("path");
 dotenv.config({ path: "./config.env" });
 
+app.use(express.static(path.resolve(__dirname, "client/build")));
 require("./db/conn");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -12,8 +13,10 @@ app.use(require("./Routes/auth"));
 
 const PORT = process.env.PORT || 5000;
 
+app.get("*", function (req, res) {
+  res.sendFile(path.resolve(__dirname, "client/build", "index.html"));
+});
+
 app.listen(PORT, () => {
   console.log(`server started at port ${PORT}`);
 });
-
-// 63983565286398356528  sendgrid password
